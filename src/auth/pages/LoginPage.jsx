df1fb807611004c5050303f61474a01a30ecc86a
unknown <sistemas@TFEQUIPOLP2.Prestalana.local>
2024-05-23 16:56:21 -0600

    LoginPage.jsx: Styled Components agregados

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,93 @@
+import { useContext, useMemo, useState } from "react";
+import { useFetch } from "../../hooks/";
+import { AuthContext } from "../context/AuthContext";
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
-import { useContext, useMemo, useState } from "react";
-import { useFetch } from "../../hooks/";
-import "./styles/LoginPage.css";
-import { AuthContext } from "../context/AuthContext";
+import styled from "styled-components";
+
+
+
+const Section = styled.section`
+  min-height: 100vh;
+  background-color: var(--secondary-color);
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+`;
+
+const FormWrapper = styled.div`
+  width: 400px;
+  padding: 1.9rem 1.3rem;
+  height: auto;
+  background-color: var(--light-secondary-color);
+  border-radius: 20px;
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+  flex-direction: column; 
+
+  @media (max-width: 575.98px) {
+    width: 80%;
+  }
+`;
+
+const H2 = styled.h2`
+  margin-bottom: 1.2rem;
+  letter-spacing: 1px;
+  font-size: 30px;
+`;
+
+const UserDataWrapper = styled.div`
+  margin-bottom: 1.25rem;
+`;
+
+const ParagraphUserData = styled.p`
+  font-size: 15px;
+` ;
+
+const Form = styled.form`
+  width: 100%;
+`
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+`;
+
+const Input = styled.input`
+  padding: 0.125rem 0.4rem;
+  font-size: 14px;
+  height: 35px;
+`;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  font-size: 16px;
+  margin: 0px auto;
+  margin-top: 12px;
+  outline: none;
+  background-color: var(--primary-color);
+  border: none;
+  color: #fff;
+  padding: 0.4rem 0.4rem;
+  border-radius: 2px;
+  cursor: pointer;
+  transition: all 300ms ease-in-out;
+
+  &:hover{
+    background-color: #61a3be;
+  }
+`;
+
+const ParagraphError = styled.p`
+  margin-top: 5px;
+  font-size: 14px;
+  color: red;
+`;
+
+
 
 export const LoginPage = () => {
 
@@ -85,40 +168,40 @@ export const LoginPage = () => {
 
   return (
     <>
-      <div className="form-section flex flex-d-column">
-        <div className="form-container flex flex-d-column">
-          <h2>Login</h2>
+      <Section>
+        <FormWrapper>
+          <H2>Login</H2>
 
           {!isLoading && (
-            <div className="user-data">
-              <p>
+            <UserDataWrapper>
+              <ParagraphUserData>
                 <strong>Username:</strong> {results[0]?.login.username}
-              </p>
-              <p>
+              </ParagraphUserData>
+              <ParagraphUserData>
                 <strong>Password:</strong> {newPassword}
-              </p>
-            </div>
+              </ParagraphUserData>
+            </UserDataWrapper>
           )}
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="input-container">
-              <input type="text" {...register('username')} placeholder="Username"/>
-              <p className="error-login">{errors.username?.message}</p>
-            </div>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <InputWrapper>
+              <Input type="text" {...register('username')} placeholder="Username"/>
+              <ParagraphError>{errors.username?.message}</ParagraphError>
+            </InputWrapper>
 
-            <div className="input-container">
-              <input {...register('password')} placeholder="Password"/>
-              <p className="error-login">{errors.password?.message}</p>
-            </div>
+            <InputWrapper>
+              <Input {...register('password')} placeholder="Password"/>
+              <ParagraphError>{errors.password?.message}</ParagraphError>
+            </InputWrapper>
 
             {
-              (!isFormValid.isValid) && <p className="error-login">{isFormValid.errorLogin}</p>
+              (!isFormValid.isValid) && <ParagraphError>{isFormValid.errorLogin}</ParagraphError>
             }
 
-            <button type="submit">Login</button>
-          </form>
-        </div>
-      </div>
+            <SubmitButton type="submit">Login</SubmitButton>
+          </Form>
+        </FormWrapper>
+      </Section>
     </>
   );
 };
