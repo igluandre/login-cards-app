42644afb1cce9eeeead270e0625fbc4a863afc5f
unknown <sistemas@TFEQUIPOLP2.Prestalana.local>
2024-05-23 13:04:31 -0600

    Navbar.jsx: styled-components added

--- src/CardsPage/components/Navbar.jsx
+++ src/CardsPage/components/Navbar.jsx
@@ -1,20 +1,61 @@
 import { useContext } from "react";
-import "../styles/Navbar.css";
 import { AuthContext } from "../../auth/context/AuthContext";
+
+import styled from "styled-components";
+
+const Header = styled.header`
+    height: 70px;
+    background-color: var(--primary-color);
+    padding: 5px 0px;
+`;
+
+const Container = styled.div`
+    max-width: 1500px;
+    margin: 0 auto;
+    padding: 0 40px;
+    overflow: auto;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    height: 100%;
+`;
+
+const Nav = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    gap: 30px; 
+`
+
+const NavbarButton = styled.button`
+    border-radius: 5px;
+    padding: 5px 14px;
+    background-color: var(--light-color);
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 300ms linear;
+    letter-spacing: 1px;
+
+    &:hover {
+      opacity: 0.8;
+    }
+`;
 
 export const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
 
   return (
     <>
-      <header className="navbar">
-        <div className="container flex">
-          <nav className="flex">
+      <Header>
+        <Container>
+          <Nav className="flex">
             <p>Hello, {user.username}</p>
-            <button onClick={() => logout()}>Logout</button>
-          </nav>
-        </div>
-      </header>
+            <NavbarButton onClick={() => logout()}>Logout</NavbarButton>
+          </Nav>
+        </Container>
+      </Header>
     </>
   );
 };
