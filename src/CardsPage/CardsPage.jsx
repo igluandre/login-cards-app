029356b2539d9dcbe7b5b655e2f2ef80721b66de
unknown <sistemas@TFEQUIPOLP2.Prestalana.local>
2024-05-23 13:08:50 -0600

    CardsPage.jsx: styled-components added

--- src/CardsPage/CardsPage.jsx
+++ src/CardsPage/CardsPage.jsx
@@ -1,7 +1,25 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useFetch } from "../hooks/useFetch";
 import { Card, Navbar, Spinner } from "./components";
-import "./styles/CardsPage.css";
+
+import styled from "styled-components";
+
+const Section = styled.section`
+  margin: 30px 0px;
+`;
+
+const Container = styled.div`
+  max-width: 1500px;
+  margin: 0 auto;
+  padding: 0 40px;
+  overflow: auto;
+`;
+
+const CardWrapper = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.9rem;
+`;
 
 export const CardsPage = () => {
   const randomNumber = useMemo(() => {
@@ -25,9 +43,9 @@ export const CardsPage = () => {
     <>
       <Navbar />
 
-      <section className="cards-section">
-        <div className="cards-container container">
-          <ul className="cards-wrapper">
+      <Section>
+        <Container>
+          <CardWrapper>
             {isLoading ? (
               <Spinner />
             ) : (
@@ -43,9 +61,9 @@ export const CardsPage = () => {
                 />
               ))
             )}
-          </ul>
-        </div>
-      </section>
+          </CardWrapper>
+        </Container>
+      </Section>
     </>
   );
 };
